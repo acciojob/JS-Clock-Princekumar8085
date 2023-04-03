9fc6e2e15ab26ba6ff73ae9fae87e27d97dc538e
Princekumar8085 <114429163+Princekumar8085@users.noreply.github.com>
2023-04-03 14:42:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const hourHand = document.querySelector(".hour-hand");
-const minuteHand = document.querySelector(".minute-hand");
+const minuteHand = document.querySelector(".min-hand");
 const secondHand = document.querySelector(".second-hand");
 
 function rotateHands() {
